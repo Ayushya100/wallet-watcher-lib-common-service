1ca5ff71c14dbddb263ff257345687922a30d47c
Ayushya <jaiswalayushya@gmail.com>
2024-03-15 22:48:52 +0530

    Added logger to library

--- src/utils/index.js
+++ src/utils/index.js
@@ -2,10 +2,12 @@
 
 import { ApiError, buildApiError } from './ApiError.js';
 import { ApiResponse, buildApiResponse } from './ApiResponse.js';
+import logger from './logger.js';
 
 export {
     ApiError,
     ApiResponse,
     buildApiError,
-    buildApiResponse
+    buildApiResponse,
+    logger
 };
