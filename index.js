35e7f94d2a18fdcc4444b678647e5778da7c2187
Ayushya <jaiswalayushya@gmail.com>
2024-03-15 21:06:52 +0530

    Added buildApiResponse util to library

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 'use strict';
 
 import { responseCodes, responseMessage } from './src/assets/response/response-codes.js';
-import { ApiError, ApiResponse, buildApiError } from './src/utils/index.js';
+import { ApiError, ApiResponse, buildApiError, buildApiResponse } from './src/utils/index.js';
 
 export {
     ApiError,
     ApiResponse,
     responseCodes,
     responseMessage,
-    buildApiError
+    buildApiError,
+    buildApiResponse
 };
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,10 +1,11 @@
 'use strict';
 
 import { ApiError, buildApiError } from './ApiError.js';
-import ApiResponse from './ApiResponse.js';
+import { ApiResponse, buildApiResponse } from './ApiResponse.js';
 
 export {
     ApiError,
     ApiResponse,
-    buildApiError
+    buildApiError,
+    buildApiResponse
 };
