fbb0daca47f3644582c41196a13d76727bb25bb7
Ayushya <jaiswalayushya@gmail.com>
2024-03-17 15:43:11 +0530

    Updated logger in library

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -4,23 +4,24 @@ import winston from 'winston';
 
 const buildTransporter = () => {
     return [
-        new winston.transports.Console({
-            format: winston.format.simple()
-        })
+        new winston.transports.Console()
     ];
 }
 
-const buildFormat = (msg) => {
+const buildFormat = (label) => {
     return winston.format.combine(
         winston.format.timestamp(),
         winston.format.prettyPrint(),
         winston.format.colorize({ all: true }),
-        winston.format.label({ label: msg })
+        winston.format.label({ label: label }),
+        winston.format.printf(info => {
+            return `${info.timestamp} - ${info.level} [${info.label}]: ${info.message}`;
+        })
     );
 }
 
-const logger = (msg = '') => {
-    const format = buildFormat(msg);
+const logger = (label = '') => {
+    const format = buildFormat(label);
     const transport = buildTransporter();
 
     return winston.createLogger({
