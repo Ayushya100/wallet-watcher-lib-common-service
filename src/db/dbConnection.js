d7a09f596cc2e77329bf0df45865f4f71781cae6
Ayushya <jaiswalayushya@gmail.com>
2024-03-23 13:22:11 +0530

    Updated userModel and dbConnection

--- src/db/dbConnection.js
+++ src/db/dbConnection.js
@@ -9,7 +9,7 @@ const log = logger('db-connection');
 const dbConnection = async() => {
     try {
         const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`);
-        log.info(`Mongo DB connection established. DB HOST: ${connectionInstance.connection.host}`);
+        log.info(`Mongo DB connection established. DB HOST: ${connectionInstance.connection.host}:${connectionInstance.connection.port}/${connectionInstance.connection.name}`);
     } catch (err) {
         log.error(`MongoDB connection error: ${err}`);
         process.exit(1); // Exit the app on db connection error
